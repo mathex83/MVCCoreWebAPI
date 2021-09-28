57e1def5abc910f0622bc0e3370a9a9a08440cb6
Martin <marnor83@gmail.com>
2021-09-28 14:59:08 +0200

    Testmodel and its controller added

---
+++ ProjectApi/ProjectApi/Controllers/MatchController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectApi.Controllers
+{
+	public class MatchController : Controller
+	{
+		public IActionResult Create()
+		{
+			return View();
+		}
+	}
+}
