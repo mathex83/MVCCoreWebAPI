25f690cdb5e83cfc8b252f062a9219869b871e5b
Martin <marnor83@gmail.com>
2021-09-30 17:16:50 +0200

    changes

---
+++ ProjectApi/SoccerAPI/Models/JsonLeague.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoccerAPI.Models
+{
+	public class JsonLeague
+	{
+		public int Id { get; set; }
+		public bool Active { get; set; }
+		public string Type { get; set; }
+		public string Legacy_id { get; set; }
+		public int Country_id { get; set; }
+		public string Logo_path { get; set; }
+		public string Name { get; set; }
+		public bool IsCup { get; set; }
+		public bool IsFriendly { get; set; }
+		public int CurrentSeasonId { get; set; }
+		"": 18369, "current_round_id": 247437, "current_stage_id": 77453684, "live_standings": true, "coverage": { "predictions": true, "topscorer_goals": true, "topscorer_assists": true, "topscorer_cards": true
+	}
+}
