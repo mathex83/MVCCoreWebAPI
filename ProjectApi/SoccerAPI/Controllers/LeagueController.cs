28f2eee33db555fb458894a935a63dc194097a05
Martin <marnor83@gmail.com>
2021-10-01 21:12:06 +0200

    working fixturelist to cshtml

--- ProjectApi/SoccerAPI/Controllers/LeagueController.cs
+++ ProjectApi/SoccerAPI/Controllers/LeagueController.cs
@@ -13,14 +13,12 @@ namespace SoccerAPI.Controllers
 	{
 		public IActionResult League()
 		{
-
-			string apiString = "api_token=2WPKee3pGNARMqtnhznGwIRCHMfIGvtL2xGQuMBrsNGpFZzGJU7xzlsdTo9G";
-			RestClient client = new RestClient("https://soccer.sportmonks.com/api/v2.0/leagues/501?" + apiString);
+			Api api = new Api();			
+			RestClient client = new RestClient(api.BaseAddress + "leagues/501?" + api.Key);
 			client.Timeout = -1;
 			RestRequest request = new RestRequest(Method.GET);
 			IRestResponse response = client.Execute(request);
-			JsonLeague dataObject = (JsonLeague)JObject.Parse(response.Content)["data"].ToObject((typeof(JsonLeague)));
-
+			League dataObject = (League)JObject.Parse(response.Content)["data"].ToObject((typeof(League)));
 			ViewBag.Message = dataObject;
 			return View();
 		}
